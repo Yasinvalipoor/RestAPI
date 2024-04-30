93b467c2c2d0bcc132794fe9161d74e495862a23
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-04-30 19:48:41 +0330

    new change

---
+++ RestAPI.WebAPI.MVC.ThirdPart/Controllers/MyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.WebAPI.MVC.ThirdPart.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+
+    public class MyController : ControllerBase
+    {
+        public string GetName()
+        {
+            return "Hello Every Body";
+        }
+    }
+}
+ 
