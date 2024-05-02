9069052de22e5b02b55d1bfa29a7852f6fc8727f
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-02 20:40:49 +0330

    new change

---
+++ RestAPI.WebAPI.MVC.ThirdPart/Controllers/FormatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.WebAPI.MVC.ThirdPart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    //Here we test the type of return format that should be used with "Postman" and see if the output is application/jason or text/plain.
+    public class FormatController : ControllerBase
+    {
+        //text/plain
+        [HttpGet("str")]
+        public string GetString() => "Hi";
+
+        //application/jason
+        [HttpGet("int")]
+        public int GetInt() => 1;
+
+        //application/jason
+        [HttpGet("obj")]
+        public Object GetObj() => new { Product = "Book" , Price = "10000" };
+    }
+}
